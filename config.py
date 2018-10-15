781ef6349064b3c3ebb1d7f2f84b6aa489d19f76
Adam Christie <fractos@gmail.com>
2018-10-15 12:08:38 +0100

    Get the right boolean value out of an environment variable including default if not present

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
 import os
+import distutils.util
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    DEBUG = bool(os.environ.get('DEBUG', 'True'))
+    DEBUG = bool(distutils.util.strtobool(os.environ.get('DEBUG', 'True')))
 
 
 class TestConfig(Config):
